43d2bae60b40c9827fc3e1d659f2179a44c62cbd
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2019-12-30 11:42:45 -0300

    update teste.rb

--- testes alura/maior_menor.rb
+++ testes alura/maior_menor.rb
@@ -1,7 +1,6 @@
 puts "Bem vindo ao jogo da Adivinhação"
 puts "Qual seu nome? "
 nome = gets
-
 puts "\n\n\n"
 puts "Começaremos o jogo para você, " + nome
 
@@ -9,19 +8,21 @@ numero_secreto = 175
 puts "Escolhido"
 puts "\n\n\n"
 
-puts "Tentativa 1"
-chute = gets
-puts "Será que você acertou?" + chute
+for dedos in 1..3
+    puts "Tentativa " + dedos.to_s
+    chute = gets
+    puts "Será que você acertou?" + chute
 
-acertou = numero_secreto == chute.to_i
+    acertou = numero_secreto == chute.to_i
 
-if acertou
-    puts"Acertou!!!"
-else
-    maior = numero_secreto > chute.to_i
-    if maior
-        puts ("O número secreto é maior")
+    if acertou
+        puts"Acertou!!!"
     else
-        puts ("O número secreto é menor")
+        maior = numero_secreto > chute.to_i
+        if maior
+            puts ("O número secreto é maior")
+        else
+            puts ("O número secreto é menor")
+        end
     end
 end
