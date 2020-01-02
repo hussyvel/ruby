0a3edff402f3751937d2992b7d483d2b13ad13a4
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2020-01-02 09:52:17 -0300

    update of the programs

--- testes alura/maior_menor.rb
+++ testes alura/maior_menor.rb
@@ -12,24 +12,30 @@ def sorteia_numero_secreto
     puts "\n\n\n"
     return numero
 end
-da_boas_vindas
-numero_secreto = sorteia_numero_secreto
 
-limite_de_tentativas = 5
-
-
-for tentativa in 1..limite_de_tentativas
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "\n\n\n\n"
     puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número: "
     chute = gets
     puts "Será que você acertou?" + chute
+    chute.to_i
+end
 
-    acertou = numero_secreto == chute.to_i
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+
+    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    acertou = numero_secreto == chute
 
     if acertou
         puts"Acertou!!!"
         break
     else
-        maior = numero_secreto > chute.to_i
+        maior = numero_secreto > chute
         if maior
             puts ("O número secreto é maior")
         else
--- testes alura/teste.rb
+++ testes alura/teste.rb
@@ -1,8 +1,47 @@
-var1 = 8
-var2 = var1
-puts var1
-puts var2
-puts ''
-var1 = 'oito'
-puts var1
-puts var2
+def boas_vindas 
+    puts "Bem vindo ao jogo da Adivinhação"
+    puts "Qual é o seu nome? "
+    nome = gets
+    puts "\n\n\n"
+    puts "Começaremos o jogo para você " + nome
+end
+
+def sorteio
+    numero = 175
+    puts "Escolhido"
+    puts "\n\n\n\n"
+    return numero
+end
+
+def pede_um_numero(tentativa, limite_de_tentativas)
+    puts "\n\n\n\n"
+    puts "Tentativas " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Entre com o número: "
+    chute = gets
+    puts "Será que vocẽ acertou? " + chute
+    chute.to_i
+end
+
+boas_vindas
+numero_secreto = sorteio
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+
+    chute = pede_um_numero(tentativa, limite_de_tentativas)
+    acertou = numero_secreto == chute
+
+    if acertou
+        puts "Acertou parabéns"
+        break
+    else
+        maior = numero_secreto > chute
+        if maior
+            puts"O número secreto é maior"
+        else
+            puts "O número secreto é menor"
+        end
+    end
+    puts "Fim do jogo!!!"
+end
+
