0feb3298ac78f72c7574a2e818dffb929ae76b23
Hussyvel Ribeiro <hussyribeiro@hotmail.com>
2020-02-23 17:42:58 -0300

    Update de programas em ruby

---
+++ testes alura/lacofor.rb
@@ -0,0 +1,3 @@
+for dedos in 1..3
+    puts ("Contando de 1 até ") + dedos.to_s
+end
--- testes alura/teste2.rb
+++ testes alura/teste2.rb
@@ -1,24 +1,9 @@
-chutes = []
-chute = 176
-tentativa = 1
-chutes[tentativa - 1] = chute
-puts chute
+puts 175 == 175
+puts 175 == 174
+puts "175" == 175
+puts "175" == "175"
+puts "175".to_i == 175
 
-chute = 88
-tentativa = 2
-chutes[tentativa - 1] = chute
-puts chute
-
-chute = 190
-tentativa = 3
-chutes[tentativa - 1] = chute
-puts chute
-
-for contador in 1..tentativa
-    puts "Chute " + chutes[contador - 1].to_s
-end
-
-puts "con""cat""en""at""ion"
-puts "This string contains" " no newlines."
-
-6.times { print "Hussyvel"}
+puts
+puts 175 != 174
+puts 175 != 175
